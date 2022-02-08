26bf011b76df58b889fda8d0c49197d5ff94e2f7
032-Abhishek Choksi <19bmiit032@gmail.com>
2022-02-08 19:33:43 +0530

    Checkbox Application @ 2022 #032AbhishekChoksi. All rights reserved.

--- app/src/main/java/com/abhishekchoksi/checkboxapplication/MainActivity.java
+++ app/src/main/java/com/abhishekchoksi/checkboxapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.abhishekchoksi.checkboxapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void chechButtonClick(View view){
+        boolean isChecked = ((CheckBox) view).isChecked();
+        switch (view.getId()){
+            case R.id.chkPlaying:
+                if(isChecked) {
+                    Toast.makeText(this, "Playing is Checked.", Toast.LENGTH_SHORT).show();
+                } else{
+                    Toast.makeText(this, "Playing is Uncheckd.", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.chkSinging:
+                if(isChecked){
+                    Toast.makeText(this, "Singing is Checked.", Toast.LENGTH_SHORT).show();
+                } else{
+                    Toast.makeText(this, "Singing is Unchecked.", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.chkLearning:
+                if(isChecked){
+                    Toast.makeText(this, "Learning is Checked.", Toast.LENGTH_SHORT).show();
+                } else{
+                    Toast.makeText(this, "Learning is Unchecked.", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 }
